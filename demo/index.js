dc562f3235d7ff672842bf0f0309f42e23cbf899
RoCky Wu <rwu@cepave.com>
2016-06-05 10:07:27 +0800

    update demo page

--- demo/index.js
+++ demo/index.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Basic example</h2>
-        <p>Works with bootstrap</p>
+        <p>Works with Bootstrap buttons</p>
 
         <div className="btn-wrap">
           <Ripples>
@@ -56,6 +56,14 @@ class App extends React.Component {
             <span className="btn btn-default">Default</span>
           </Ripples>
         </div>
+
+        <h2>The normal text content</h2>
+        <Ripples color="#c9302c" during={1200}>
+          <blockquote>
+            Ripple touch effect was introduced with material design in Android 5.0 (API level 21).
+            Touch feedback in material design provides an instantaneous visual confirmation at the point of contact when users interact with UI elements. For example, buttons now display a ripple effect when they are touched-this is the default touch feedback animation in Android 5.0. Ripple animation is implemented by the new RippleDrawable class. The ripple effect can be configured to end at the bounds of the view or extend beyond the bounds of the view.
+          </blockquote>
+        </Ripples>
       </div>
     )
   }
