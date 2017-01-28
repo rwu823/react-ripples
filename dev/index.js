77f5c8a2d14bc298df48fdfac4c8646ffa62d3ce
RoCky Wu <rocky823@gmail.com>
2017-01-28 09:41:28 +0800

    rename to `dev/`

---
+++ dev/index.js
@@ -0,0 +1,9 @@
+import React, {} from 'react'
+import { render } from 'react-dom'
+import App from './app'
+
+render(<App />, document.querySelector('#app'))
+
+if (module.hot) {
+  module.hot.accept()
+}
