e6f0060abdb84a9cb042b577a917163750e019f1
Rocky Wu <rocky@17.media>
2019-05-27 17:29:46 +0800

    Omit 'style'

--- src/withAttrs.tsx
+++ src/withAttrs.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
 const withAttrs = <P, Element>(Component: React.ComponentType<P>) => (
-  props: P & React.HTMLAttributes<Element>,
+  props: P & Omit<React.HTMLAttributes<Element>, 'style'>,
 ) => <Component {...props} />
 
 export default withAttrs
