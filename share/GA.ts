6b2309b36c20241bbcacd58d0f8e808a94d4eb8f
Rocky Wu <rocky@17.media>
2019-06-01 23:46:17 +0800

    fix GA

--- share/GA.ts
+++ share/GA.ts
@@ -1,4 +1,6 @@
-const globalThis = (1, eval)('this') // eslint-disable-line no-eval
+/* eslint-disable camelcase, prefer-rest-params, no-eval */
+
+const globalThis = (1, eval)('this')
 
 declare global {
   interface Window {
@@ -10,21 +12,25 @@ declare global {
   }
 }
 
-/* eslint-disable camelcase */
 type PageView = Partial<{
   page_title: string
   page_path: string
   send_page_view: boolean
 }>
-/* eslint-enable */
-
-globalThis.dataLayer = globalThis.dataLayer || [['js', new Date()]]
 
 export default class GA {
   private id: string
 
   constructor(id: string) {
     this.id = id
+
+    globalThis.dataLayer = globalThis.dataLayer || []
+    globalThis.gtag = (...args: any[]) => {
+      globalThis.dataLayer.push(args)
+    }
+    globalThis.gtag('js', new Date())
+
+    globalThis.gtag('config', id)
   }
 
   pageView(options: PageView = {}) {
@@ -32,6 +38,7 @@ export default class GA {
       page_title: document.title,
       page_path: window.location.pathname,
     })
-    globalThis.dataLayer.push(['config', this.id, options])
+
+    globalThis.gtag('config', this.id, options)
   }
 }
